eaf0eb65dc3eac3e41135fb8fcaf237e816f845d
Abhinav110897 <abhinavsrivastava10897@gmail.com>
2021-10-29 16:25:37 +0530

    Adding Initial POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/PetClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.PetClinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/PetClinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.PetClinic.model;
+
+public class Vet extends Person{
+}
